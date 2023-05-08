0728b933769b9895c38fff71f76a9ee75054b3f3
peterxcli <peterxcli@users.noreply.github.com>
2023-05-08 04:32:19 +0000

    deploy: d8f46037b77bc681e9bb52a05fa595379402567d

---
+++ _next/static/FrG5PP8eNz87DbVuxFizX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/217-31eb76dcf8640ab3.js","static/css/2ae997430c6030b7.css","static/chunks/pages/index-2d06884d93059db0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
